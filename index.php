becee112f2c9451373117bb27996712400953564
dricaurte <daricaurte@gmail.com>
2017-01-12 00:12:45 -0500

    Revamp Launch
    Time to mix things up

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html> 
+<head>
+   <?php include 'resources/head.php';?>
+</head>
+<body>
+  <div class= "container full">
+    <div class= "row centered full">
+      <div class= "col-xs-10 centered full">
+        <img class= "underconstruction" src="images/underconstruction.gif" title= "Under Construction" alt= "underconstruction">
+      </div>
+    </div> 
+  </div> 
+  <!--Footer Section-->
+  <?php include 'resources/footer.php';?>
+  <!--Footer Section-->
+<!--Scripts-->  
+  <script src="js/bootstrap.min.js" type="text/javascript"></script>
+<!--Scripts-->
+</body>
+</html>
+
